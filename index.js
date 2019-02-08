9596ff560b51cbb7e386defb12de47a4a85fb63d
fono09 <fono@fono.jp>
2019-02-08 23:39:01 +0900

    Auto recconect

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var WebSocketClient = require('websocket').client
-var wsc = new WebSocketClient();
+var wsc
 var config = require('config')
 var htmlDecode = require('unescape')
 
@@ -15,79 +15,80 @@ var twc = new Twitter({
 TARGET_ACCT =  config.target_acct
 
 
-
-wsc.on('connectFailed', e => {
-    console.log('Connection Error: ' + e.toString())
-})
-
-wsc.on('connect', connection => {
-    connection.on('error', e => {
+var connect = () => {
+    wsc = new WebSocketClient()
+    wsc.on('connectFailed', e => {
         console.log('Connection Error: ' + e.toString())
     })
 
-    connection.on('close', () => {
-        console.log('Connection Closed')
-    })
+    wsc.on('connect', connection => {
+        connection.on('error', e => {
+            console.log('Connection Error: ' + e.toString())
+        })
 
-    connection.on('message', m => {
-        if(m.type !== 'utf8'){
-            return
-        }
+        connection.on('close', () => {
+            console.log('Connection Closed')
+            setTimeout(connect, 5000)
+        })
 
-        var wsEvent
-        try {
-            wsEvent = JSON.parse(m.utf8Data)
-        } catch (e) {
-            console.log('JSON.parse Error: ' + e.toString())
-            wsEvent = null
-        }
-         
-        if(wsEvent == null || wsEvent.event != 'update'){
-            return
-        }
-        
-        var payload
-        try {
-            payload = JSON.parse(wsEvent.payload)
-        } catch (e) {
-            console.log('JSON.parse Error: ' + e.toString())
-            payload = null
-        }
-        if(payload == null){
-            return
-        }
-        
-        if(payload.account.acct != TARGET_ACCT){
-            return
-        }
-
-        if('tags' in payload == false){
-            return
-        }
-
-        if(payload.tags.find(e => e.name == 'ttr') == undefined){
-            return
-        }
-
-        var url_expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
-
-        var tweet_text = htmlDecode(payload.content.replace(/<\/?(\w+)( (\w+)="([^"]*)")*( \/)?>/g,'').replace(url_expression, ''))
-        var urls = payload.content.match(url_expression).filter((value, index, array) => array.indexOf(value) === index).filter(e => /https:\/\/ma\.fono\.jp\/tags/.test(e) == false)
-
-        if(urls != null){
-            tweet_text = tweet_text + ' ' + urls.join(' ')
-        }
-        console.log(tweet_text)
-
-        twc.post('statuses/update', {status: tweet_text}, (error, tweet, response) => {
-            if(!error){
-                console.log(tweet)
+        connection.on('message', m => {
+            if(m.type !== 'utf8'){
+                return
             }
+
+            var wsEvent
+            try {
+                wsEvent = JSON.parse(m.utf8Data)
+            } catch (e) {
+                console.log('JSON.parse Error: ' + e.toString())
+                wsEvent = null
+            }
+             
+            if(wsEvent == null || wsEvent.event != 'update'){
+                return
+            }
+            
+            var payload
+            try {
+                payload = JSON.parse(wsEvent.payload)
+            } catch (e) {
+                console.log('JSON.parse Error: ' + e.toString())
+                payload = null
+            }
+            if(payload == null){
+                return
+            }
+            
+            if(payload.account.acct != TARGET_ACCT){
+                return
+            }
+
+            if('tags' in payload == false){
+                return
+            }
+
+            if(payload.tags.find(e => e.name == 'ttr') == undefined){
+                return
+            }
+
+            var url_expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+
+            var tweet_text = htmlDecode(payload.content.replace(/<\/?(\w+)( (\w+)="([^"]*)")*( \/)?>/g,'').replace(url_expression, ''))
+            var urls = payload.content.match(url_expression).filter((value, index, array) => array.indexOf(value) === index).filter(e => /https:\/\/ma\.fono\.jp\/tags/.test(e) == false)
+
+            if(urls != null){
+                tweet_text = tweet_text + ' ' + urls.join(' ')
+            }
+            console.log(tweet_text)
+
+            twc.post('statuses/update', {status: tweet_text}, (error, tweet, response) => {
+                if(!error){
+                    console.log(tweet)
+                }
+            })
         })
     })
-})
 
-wsc.connect('wss://ma.fono.jp/api/v1/streaming?access_token=' + config.mastodon_access_token + '&stream=user', null)
-
-
-
+    wsc.connect('wss://ma.fono.jp/api/v1/streaming?access_token=' + config.mastodon_access_token + '&stream=user', null)
+}
+connect()
